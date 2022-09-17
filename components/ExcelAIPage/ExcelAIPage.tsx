254574bda2022cf95228fe1027e28337b893f5c4
Emanuel Alvarado <emanuelalvaradog@gmail.com>
2022-09-17 02:41:06 -0500

    stripe integration

--- components/ExcelAIPage/ExcelAIPage.tsx
+++ components/ExcelAIPage/ExcelAIPage.tsx
@@ -117,34 +117,33 @@ export function ExcelAIPage() {
           >
             Mi cuenta
           </h1>
-          <div className={styles.item} onClick={handleSignOut}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="#8C9099"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
-              />
-            </svg>
-
-            <h1 className={styles.itemNotSelected}>Cerrar sesión</h1>
-          </div>
-
-          <div className={styles.secondPannel}>
-            {currentScreen === Screens.CREATE_FORMULA ? (
-              <CreateFormulaPage />
-            ) : (
-              <></>
-            )}
-            {currentScreen === Screens.HISTORY ? <HistoryPage /> : <></>}
-            {currentScreen === Screens.ACCOUNT ? <MyAccountPage /> : <></>}
-          </div>
         </div>
+        <div className={styles.item} onClick={handleSignOut}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="#8C9099"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
+            />
+          </svg>
+
+          <h1 className={styles.itemNotSelected}>Cerrar sesión</h1>
+        </div>
+      </div>
+      <div className={styles.secondPannel}>
+        {currentScreen === Screens.CREATE_FORMULA ? (
+          <CreateFormulaPage />
+        ) : (
+          <></>
+        )}
+        {currentScreen === Screens.HISTORY ? <HistoryPage /> : <></>}
+        {currentScreen === Screens.ACCOUNT ? <MyAccountPage /> : <></>}
       </div>
     </div>
   );
