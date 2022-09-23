146b26838c3864a56b6ef2059a6ac097cb07778f
cruzryan <32502734+cruzryan@users.noreply.github.com>
2022-09-22 22:23:43 -0500

    added persistance

--- components/AuthPage/LoginPage.tsx
+++ components/AuthPage/LoginPage.tsx
@@ -1,142 +1,101 @@
 import React, { useState } from "react";
 import styles from "./Auth.module.css";
 
-import { getAuth, signInWithEmailAndPassword, setPersistence, browserSessionPersistence  } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword, setPersistence, browserSessionPersistence } from "firebase/auth";
 import Router from 'next/router';
-
-export function LoginPage({ registerInstead }) {
-
-    const [emailInputValue, setEmailInputValue] = React.useState("");
-    const [passwordInputValue, setPasswordInputValue] = React.useState("");
-
-    const [error, setError] = useState("");
-
 
 import { useDispatch } from "react-redux";
 import { setUserState, clearUserState } from "../../store/slices/userSlice";
 
 export function LoginPage({ registerInstead }) {
-  const [emailInputValue, setEmailInputValue] = React.useState("");
-  const [passwordInputValue, setPasswordInputValue] = React.useState("");
-  const [error, setError] = useState("");
-  const dispatch = useDispatch();
+    const [emailInputValue, setEmailInputValue] = React.useState("");
+    const [passwordInputValue, setPasswordInputValue] = React.useState("");
+    const [error, setError] = useState("");
+    const dispatch = useDispatch();
 
-  const validated = () => {
-    if (emailInputValue === "") {
-      setError("Recuerda ingresar un correo!");
-      return false;
+    const validated = () => {
+        if (emailInputValue === "") {
+            setError("Recuerda ingresar un correo!");
+            return false;
+        }
     }
 
     const loginAccount = () => {
         if (validated()) {
             const auth = getAuth();
-            
-            console.log("AUTH: ", auth)
 
             signInWithEmailAndPassword(auth, emailInputValue, passwordInputValue)
                 .then((userCredential) => {
-                    // Signed in 
+                    // Signed in
                     const user = userCredential.user;
+                    // console.log(user);
+
+                    const userData: User = {
+                        name: user.displayName,
+                        email: user.email,
+                        uid: user.uid,
+                        isAdmin: false,
+                        plan: "Free",
+                        lastBought: 0,
+                        created: user.metadata.createdAt,
+                    };
+
+                    dispatch(setUserState(userData));
 
                     setPersistence(auth, browserSessionPersistence).then(() => {
                         return signInWithEmailAndPassword(auth, emailInputValue, passwordInputValue)
                     })
-
-                    Router.push('/excelai');
+                    Router.push("/excelai");
                 })
                 .catch((err) => {
                     const errorCode = err.code;
 
-                    switch(errorCode){
-                        case "auth/wrong-password": { setError("Usuario no encontrado, ¿lo escribiste bien?") } break;
-                        case "auth/user-not-found ": { setError("Contraseña incorrecta, ¿la escribiste bien?") } break;
+                    switch (errorCode) {
+                        case "auth/wrong-password":
+                            {
+                                setError("Usuario no encontrado, ¿lo escribiste bien?");
+                            }
+                            break;
+                        case "auth/user-not-found ":
+                            {
+                                setError("Contraseña incorrecta, ¿la escribiste bien?");
+                            }
+                            break;
                     }
                 });
         }
+    };
 
-    if (passwordInputValue === "") {
-      setError("Recuerda ingresar una contraseña!");
-      return false;
+    return (
+        <div className={styles.page}>
+            <h1>Iniciar Sesión</h1>
 
-    }
+            <input
+                type="email"
+                placeholder="Correo"
+                onChange={(e) => setEmailInputValue(e.target.value)}
+                value={emailInputValue}
+            ></input>
 
-    setError("");
-    return true;
-  };
+            <input
+                type="password"
+                placeholder="Contraseña"
+                onChange={(e) => setPasswordInputValue(e.target.value)}
+                value={passwordInputValue}
+            ></input>
 
-  const loginAccount = () => {
-    if (validated()) {
-      const auth = getAuth();
+            {error === "" ? <></> : <h2 className={styles.error}>{error}</h2>}
 
-      signInWithEmailAndPassword(auth, emailInputValue, passwordInputValue)
-        .then((userCredential) => {
-          // Signed in
-          const user = userCredential.user;
-          // console.log(user);
-
-          const userData: User = {
-            name: user.displayName,
-            email: user.email,
-            uid: user.uid,
-            isAdmin: false,
-            plan: "Free",
-            lastBought: 0,
-            created: user.metadata.createdAt,
-          };
-
-          dispatch(setUserState(userData));
-
-          Router.push("/excelai");
-        })
-        .catch((err) => {
-          const errorCode = err.code;
-
-          switch (errorCode) {
-            case "auth/wrong-password":
-              {
-                setError("Usuario no encontrado, ¿lo escribiste bien?");
-              }
-              break;
-            case "auth/user-not-found ":
-              {
-                setError("Contraseña incorrecta, ¿la escribiste bien?");
-              }
-              break;
-          }
-        });
-    }
-  };
-
-  return (
-    <div className={styles.page}>
-      <h1>Iniciar Sesión</h1>
-
-      <input
-        type="email"
-        placeholder="Correo"
-        onChange={(e) => setEmailInputValue(e.target.value)}
-        value={emailInputValue}
-      ></input>
-
-      <input
-        type="password"
-        placeholder="Contraseña"
-        onChange={(e) => setPasswordInputValue(e.target.value)}
-        value={passwordInputValue}
-      ></input>
-
-      {error === "" ? <></> : <h2 className={styles.error}>{error}</h2>}
-
-      <button onClick={() => loginAccount()} className={styles.continueBtn}>
-        Continuar
-      </button>
-      <button className={styles.forgotPasswordBtn}>
-        ¿Olvidaste tu contraseña?
-      </button>
-      <hr className={styles.smolHr} />
-      <button onClick={() => registerInstead()} className={styles.greenBtn}>
-        Crear una cuenta
-      </button>
-    </div>
-  );
+            <button onClick={() => loginAccount()} className={styles.continueBtn}>
+                Continuar
+            </button>
+            <button className={styles.forgotPasswordBtn}>
+                ¿Olvidaste tu contraseña?
+            </button>
+            <hr className={styles.smolHr} />
+            <button onClick={() => registerInstead()} className={styles.greenBtn}>
+                Crear una cuenta
+            </button>
+        </div>
+    );
 }
