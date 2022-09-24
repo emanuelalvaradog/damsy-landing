a82a6e41657d3bf71b4a0b0c388c386275e88916
cruzryan <32502734+cruzryan@users.noreply.github.com>
2022-09-23 21:56:14 -0500

    Moved everything to components

--- pages/precios.js
+++ pages/precios.js
@@ -1,100 +1,11 @@
-import React from "react";
-import styles from "../styles/Pricing.module.css";
-import Router from "next/router";
-import { HomeHeader } from "../components";
-import { stripeCheckout } from "../stripe/stripeCheckout";
-import { useDispatch, useSelector } from "react-redux";
+import { PricesPage } from "../components/PricesPage/PricesPage";
 
-export default function Precios() {
-  const dispatch = useDispatch();
-  const { uid } = useSelector((store) => store.user);
-
+function Precios({ Component, pageProps }) {
   return (
     <>
-      <HomeHeader displayLogin />
-      <div className={styles.pricing}>
-        <h1>Incrementa tu productividad</h1>
-        <p>Crea fórmulas como un experto con Damsy ExcelAI</p>
-        <section className={styles.cards}>
-          <div className={styles.card}>
-            <div className={styles.cardHeader}>
-              <h2>Plan Gratuito</h2>
-              <p>$0</p>
-            </div>
-            <div className={styles.cardBenefits}>
-              <h3>Acceso limitado a Damsy ExcelAI</h3>
-              <ul>
-                <li>Limitado a 5 fórmulas por mes</li>
-                <li>Acceso limitado a historial de fórmulas</li>
-              </ul>
-              <button
-                name="free"
-                onClick={() => {
-                  Router.push("/auth?login");
-                }}
-              >
-                Comprar
-              </button>
-            </div>
-          </div>
-          <div className={styles.card}>
-            <div className={styles.cardHeader}>
-              <h2>Plan Mensual</h2>
-              <p>$99 MXN</p>
-            </div>
-            <div className={styles.cardBenefits}>
-              <h3>Acceso completo a Damsy ExcelAI</h3>
-              <ul>
-                <li>Fórmulas ilimitadas</li>
-                <li>Explicaciones ilimitadas</li>
-                <li>Acceso completo al historial de fórmulas</li>
-              </ul>
-              <button
-                onClick={() =>
-                  stripeCheckout({
-                    lineItems: [
-                      {
-                        price: "price_1LivSQD1ZyZsk1mP95uvYcCj",
-                        quantity: 1,
-                      },
-                    ],
-                  })
-                }
-              >
-                Comprar
-              </button>
-            </div>
-          </div>
-          <div className={styles.card}>
-            <div className={styles.cardHeader}>
-              <h2>Plan Anual</h2>
-              <p>$799 MXN</p>
-            </div>
-            <div className={styles.cardBenefits}>
-              <h3>Acceso completo a Damsy ExcelAI</h3>
-              <ul>
-                <li>Fórmulas ilimitadas</li>
-                <li>Explicaciones ilimitadas</li>
-                <li>Acceso completo al historial de fórmulas</li>
-              </ul>
-              <button
-                onClick={() =>
-                  stripeCheckout({
-                    lineItems: [
-                      {
-                        price: "price_1LiwYxD1ZyZsk1mPzA3mUrL2",
-                        quantity: 1,
-                      },
-                    ],
-                  })
-                }
-              >
-                Comprar
-              </button>
-            </div>
-          </div>
-        </section>
-      </div>
+      <PricesPage />
     </>
   );
 }
+
+export default Precios;
